2a0735b3e8b6547fbc63cd0e474f0c4f8d66746a
alainpiloto <alainpiloto@gmail.com>
2021-12-31 01:18:58 -0500

    Se corrige error de importacion useState de modulo de desarrollo por lo que no era reconocido por vercel

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react/cjs/react.development';
+import React, { useEffect,useState } from 'react'
+
 import Paciente from './Paciente'
 import filter from '../assets/filter.png'
 
